3a4bbccf2b485a0fc1aa6e3f07d71ec124b1960c
Ochiabuto Kelechi Emmanuel <kcochiabuto@gmail.com>
2024-10-21 19:38:13 +0100

    Add files via upload

---
+++ SQL Location.sql
@@ -0,0 +1,16 @@
+/* Creating a LOCATIONTABLE*/
+
+Create Table Location
+(
+LocationName varchar (20),
+LocationID VARCHAR (20)
+)
+
+/*Inserting Data into the LOCATION table*/
+Insert into Location (LocationName, LocationID) VALUES ('Astoria', 'L1')
+
+Insert into Location (LocationName, LocationID) VALUES ('Hells Kitchen', 'L3')
+
+Insert into Location (LocationName, LocationID) VALUES ('Lower Manhattan', 'L2')
+
+
